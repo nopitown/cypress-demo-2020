84dc6c00ee534a94d6687580c8e65c751c5e742c
Elizabeth Manrique <ecmanrique+nopito@gmail.com>
2020-03-10 00:51:28 -0500

    Add Cypress tests

---
+++ cypress/integration/app.test.js
@@ -0,0 +1,16 @@
+describe("App", () => {
+  it("add todo", () => {
+    cy.visit("/");
+    cy.findByPlaceholderText("Enter a new todo").type("Clean your room{enter}");
+    cy.findByText("Clean your room");
+  });
+
+  it("delete todo", () => {
+    cy.visit("/");
+    cy.findByPlaceholderText("Enter a new todo").type("Clean your room{enter}");
+    cy.findAllByText("Remove")
+      .last()
+      .click();
+    cy.findByText("Clean your room").should("not.exist");
+  });
+});
